d857d9f34b24effa5b4c2e04e8ce573702d892da
teusmamf <martinsmateus382@gmail.com>
2025-03-24 14:32:21 -0300

    ajustadno url da criação imagem estrela

--- src/services/astronomy.ts
+++ src/services/astronomy.ts
@@ -8,13 +8,13 @@ interface StarChartParams {
 }
 
 export const getStarChart = async ({ date, latitude, longitude }: StarChartParams) => {
-  const response = await axios.post('/api/star_chart/star-chart', { date, latitude, longitude });
+  const response = await axios.post('/star_chart/star-chart', { date, latitude, longitude });
   console.log(response);
   
   return response.data;
 };
 
 export const getCelestialBodies = async (date: string) => {
-  const response = await axios.get(`/api/star_chart/celestial-bodies?date=${date}`);
+  const response = await axios.get(`/star_chart/celestial-bodies?date=${date}`);
   return response.data;
 };
